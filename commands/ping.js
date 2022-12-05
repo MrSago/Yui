b9d2f098ecfea2b8d612b20fb61c16f952acd0c8
MrSago <37180898+MrSago@users.noreply.github.com>
2022-12-05 03:01:14 +0300

    add ephemeral reply for ping command

--- commands/ping.js
+++ commands/ping.js
@@ -7,7 +7,7 @@ module.exports = {
         .setDescription('Replies with Pong!'),
 
     async execute(interaction) {
-        await interaction.reply('Pong!');
+        await interaction.reply({ content: 'Pong!', ephemeral: true });
     },
 };
 
