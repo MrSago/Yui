874d148f1db885c2039ac7253a694c894d004643
Alexander <37180898+MrSago@users.noreply.github.com>
2023-03-04 14:18:13 +0300

    another fixes
    Co-authored-by: Maxim <deadly536@gmail.com>

--- auctionator.js
+++ auctionator.js
@@ -1,20 +1,19 @@
+const axios = require("axios");
+const fs = require("fs");
+const { EmbedBuilder } = require("discord.js");
 
-const axios = require('axios');
-const fs = require('fs');
-const { EmbedBuilder } = require('discord.js');
+const baseUrl = "https://www.sirus.su";
+const apiItemPath = "/api/base/item/";
 
-const baseUrl = 'https://www.sirus.su';
-const apiItemPath = '/api/base/item/';
+const settingsPath = "./settings/";
+const settingsFile = settingsPath + "auctionator.json";
 
-const settingsPath = './settings/';
-const settingsFile = settingsPath + 'auctionator.json';
-
-const delay = 300000;
+const delay = 5000;
 
 const realmIdString = {
     9: "Scourge x2",
     33: "Algalon x4",
-    57: "Sirus x5"
+    57: "Sirus x5",
 };
 
 var itemsBase = {};
@@ -28,7 +27,7 @@ function initAuctionator(client) {
         fs.mkdirSync(settingsPath);
     }
     try {
-        itemsBase = JSON.parse(fs.readFileSync(settingsFile, 'utf8'))
+        itemsBase = JSON.parse(fs.readFileSync(settingsFile, "utf8"));
         console.log(`[LOG] Settings successfully loaded from ${settingsFile}`);
     } catch (error) {
         console.error(error);
@@ -36,25 +35,33 @@ function initAuctionator(client) {
     }
 
     setInterval(async () => {
-        for (const guild_id in itemsBase) {
-            for (const realm_id in itemsBase[guild_id]) {
-                if (realm_id === 'channel_id') {
-                    continue;
-                }
+        var dateGetterPromises = [];
+        Object.entries(itemsBase).forEach((guild_id, guild) => {
+            Object.entries(guild).forEach((realm_id, realm) => {
+                if (realm_id === "channel_id") return;
 
-                for (const item_id in itemsBase[guild_id][realm_id]) {
-                    await getDataItem(realm_id, item_id)
-                        .then(data => {
-                            itemsBase[guild_id][realm_id][item_id] = parseData(data);
-                        }).catch(console.error);
-                }
+                Object.keys(realm).forEach((item_id) => {
+                    dateGetterPromises.push(async () =>
+                        getDataItem(realm_id, item_id)
+                            .then((data) => {
+                                itemsBase[guild_id][realm_id][item_id] =
+                                    parseData(data);
+                            })
+                            .catch(console.error)
+                    );
+                });
+            });
+        });
+        Promise.all(dateGetterPromises).then(() => {
+            if (!fs.existsSync(settingsPath)) {
+                fs.mkdirSync(settingsPath);
             }
-        }
-
-        if (!fs.existsSync(settingsPath)) {
-            fs.mkdirSync(settingsPath);
-        }
-        fs.writeFileSync(settingsFile, JSON.stringify(itemsBase, null, 4), 'utf8');
+            fs.writeFileSync(
+                settingsFile,
+                JSON.stringify(itemsBase, null, 4),
+                "utf8"
+            );
+        });
     }, 120000);
 
     updateEmbed();
@@ -68,32 +75,47 @@ async function updateEmbed() {
         }
 
         const embedMessage = new EmbedBuilder()
-            .setColor(0x0099FF)
-            .setTitle('Сводка аукциона')
-            .setDescription(`Последнее обновление: ${new Date().toLocaleTimeString()}`);
+            .setColor(0x0099ff)
+            .setTitle("Сводка аукциона")
+            .setDescription(
+                `Последнее обновление: ${new Date().toLocaleTimeString()}`
+            );
 
         const count = Object.keys(itemsBase[guild_id]).length;
         let index = 0;
         for (const realm_id in itemsBase[guild_id]) {
             index += 1;
-            if (realm_id === 'channel_id') {
+            if (realm_id === "channel_id") {
                 continue;
             }
 
-            let item_string = '';
-            let item_price = '';
+            let item_string = "";
+            let item_price = "";
             for (const item_id in itemsBase[guild_id][realm_id]) {
-                item_string += itemsBase[guild_id][realm_id][item_id].name + '\n';
-                item_price += itemsBase[guild_id][realm_id][item_id].price + '\n';
+                item_string +=
+                    itemsBase[guild_id][realm_id][item_id].name + "\n";
+                item_price +=
+                    itemsBase[guild_id][realm_id][item_id].price + "\n";
             }
 
             try {
                 embedMessage.addFields(
-                    { name: realmIdToString(realm_id), value: item_string, inline: true },
-                    { name: 'Средняя цена', value: item_price, inline: true },
+                    {
+                        name: realmIdToString(realm_id),
+                        value: item_string,
+                        inline: true,
+                    },
+                    {
+                        name: "Средняя цена",
+                        value: item_price,
+                        inline: true,
+                    }
                 );
                 if (index < count - 1) {
-                    embedMessage.addFields({ name: '\u200B', value: '\u200B' });
+                    embedMessage.addFields({
+                        name: "\u200B",
+                        value: "\u200B",
+                    });
                 }
             } catch (error) {
                 console.error(error);
@@ -103,18 +125,23 @@ async function updateEmbed() {
 
         try {
             const channel = bot.channels.cache.get(channel_id);
-            await channel.messages.fetch({ limit: 1 })
-                .then(messages => {
+            await channel.messages
+                .fetch({
+                    limit: 1,
+                })
+                .then((messages) => {
                     let lastMessage = messages.first();
                     if (lastMessage.author.id === bot.user.id) {
-                        lastMessage.edit({ embeds: [embedMessage] });
+                        lastMessage.edit({
+                            embeds: [embedMessage],
+                        });
                     } else {
-                        channel.send({ embeds: [embedMessage] });
+                        channel.send({
+                            embeds: [embedMessage],
+                        });
                     }
                 });
-        } catch (error) {
-            console.error (error);
-        }
+        } catch (e) {}
     }
 
     setTimeout(updateEmbed, delay);
@@ -126,7 +153,7 @@ function getChannelId(guild_id) {
         return undefined;
     }
     if (itemsBase[guild_id].channel_id === undefined) {
-        return undefined;;
+        return undefined;
     }
     return itemsBase[guild_id].channel_id;
 }
@@ -154,44 +181,45 @@ async function addItem(guild_id, realm_id, item_id) {
     }
 
     const data = await getDataItem(realm_id, item_id);
+    console.log(data);
+    if (!data) {
+        return undefined;
+    }
     const parsed = parseData(data);
     itemsBase[guild_id][realm_id][item_id] = parsed;
     return parsed;
 }
 
 async function getDataItem(realm_id, item_id) {
-    const itemUrl = baseUrl + apiItemPath + item_id + '/' + realm_id;
-    let data = undefined;
-
-    await axios.get(itemUrl, {
-        headers: { 'accept-encoding': null },
-        cache: true
-    }).then(response => {
-        data = response.data;
-    }).catch(error => {
-        const throwMsg = `Предмет с Id ${item_id} из реалма ${realmIdToString(realm_id)} в базе не найден`;
-        console.error(error.message);
-        console.log(`[WARNING] ${throwMsg}`)
-        throw new Error(throwMsg);
-    });
-    
-    return data;
+    const itemUrl = baseUrl + apiItemPath + item_id + "/" + realm_id;
+    return await axios
+        .get(itemUrl, {
+            headers: {
+                "accept-encoding": null,
+            },
+            cache: true,
+        })
+        .then((response) => response.data)
+        .catch((_) => undefined);
 }
 
 function parseData(data) {
-    let name = data['item']['name'];
+    let name = data["item"]["name"];
     let price = undefined;
 
     try {
-        price = (Math.round(data['auctionhouse']['avg'] / 100) / 100).toString();
+        price = (
+            Math.round(data["auctionhouse"]["avg"] / 100) / 100
+        ).toString();
     } catch {
-        console.log(`[WARNING] No data from auction for item ${name} (${data['item']['entry']})`);
-        price = 'Not available';
+        console.log(
+            `[WARNING] No data from auction for item ${name} (${data["item"]["entry"]})`
+        );
+        price = "Not available";
     }
 
     return { name: name, price: price };
 }
-
 
 module.exports = {
     initAuctionator: initAuctionator,
@@ -199,5 +227,5 @@ module.exports = {
     getChannelId: getChannelId,
     realmIdToString: realmIdToString,
     setAucChannel: setAucChannel,
-    addItem: addItem
+    addItem: addItem,
 };
--- commands/additem.js
+++ commands/additem.js
@@ -1,41 +1,51 @@
-
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
-const { updateEmbed, realmIdToString, addItem } = require('../auctionator.js');
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const { updateEmbed, realmIdToString, addItem } = require("../auctionator.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('additem')
-        .setDescription('Добавить предмет в список')
+        .setName("additem")
+        .setDescription("Добавить предмет в список")
         .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
-        .addIntegerOption(option =>
-            option.setName('realm_id')
-                .setDescription('Выберите реалм')
+        .addIntegerOption((option) =>
+            option
+                .setName("realm_id")
+                .setDescription("Выберите реалм")
                 .setRequired(true)
                 .setChoices(
-                    { name: 'Scourge x2', value: 9 },
-                    { name: 'Algalon x4', value: 33 },
-                    { name: 'Sirus x5', value: 57 }))
-        .addIntegerOption(option =>
-            option.setName('item_id')
-                .setDescription('Id предмета')
-                .setRequired(true)),
+                    { name: "Scourge x2", value: 9 },
+                    { name: "Algalon x4", value: 33 },
+                    { name: "Sirus x5", value: 57 }
+                )
+        )
+        .addIntegerOption((option) =>
+            option
+                .setName("item_id")
+                .setDescription("Id предмета")
+                .setRequired(true)
+        ),
 
     async execute(interaction) {
         const guild_id = interaction.guild.id;
-        const realm_id = interaction.options.getInteger('realm_id');
-        const item_id = interaction.options.getInteger('item_id');
+        const realm_id = interaction.options.getInteger("realm_id");
+        const item_id = interaction.options.getInteger("item_id");
 
-        addItem(guild_id, realm_id, item_id)
-            .then(addedItem => {
-                if (addedItem === undefined) {
-                    return;
-                }
-                updateEmbed();
-                const message = `${addedItem.name} (id: ${item_id}) из реалма ${realmIdToString(realm_id)} ` +
-                    `успешно добавлен в список\nТекущая средняя цена: ${addedItem.price}`;
-                interaction.reply(message);
-            }).catch(error => {
-                interaction.reply({ content: error.message, ephemeral: true });
+        try {
+            const addedItem = await addItem(guild_id, realm_id, item_id);
+            if (!addedItem) {
+                return;
+            }
+            updateEmbed();
+            const message =
+                `${addedItem.name} (id: ${item_id}) из реалма ${realmIdToString(
+                    realm_id
+                )} ` +
+                `успешно добавлен в список\nТекущая средняя цена: ${addedItem.price}`;
+            await interaction.reply(message);
+        } catch (error) {
+            await interaction.reply({
+                content: error.message,
+                ephemeral: true,
             });
-    }
+        }
+    },
 };
